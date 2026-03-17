9adb02f8ed55f787d503ae2f8dc06a9615b684e3
frisk-scratch <yuniaochaoyang@gmail.com>
2026-03-17 17:26:00 +0900

    Create ProjectInfo.cs

---
+++ src/Core/ProjectInfo.cs
@@ -0,0 +1,45 @@
+using System.Xml.Linq;
+
+namespace csharpcompile.Core;
+
+public class ProjectInfo
+{
+    public string Name { get; set; } = "";
+    public string Framework { get; set; } = "net8.0";
+    public string ProjectPath { get; set; } = "";
+
+    public static ProjectInfo Load(string path)
+    {
+        string? csproj = null;
+
+        if (Directory.Exists(path))
+        {
+            csproj = Directory.GetFiles(path, "*.csproj").FirstOrDefault();
+        }
+        else if (File.Exists(path))
+        {
+            csproj = path;
+        }
+
+        if (csproj == null)
+            throw new Exception("No .csproj found");
+
+        var doc = XDocument.Load(csproj);
+        var ns = doc.Root?.Name.Namespace;
+
+        var name =
+            doc.Descendants(ns + "AssemblyName").FirstOrDefault()?.Value ??
+            Path.GetFileNameWithoutExtension(csproj);
+
+        var framework =
+            doc.Descendants(ns + "TargetFramework").FirstOrDefault()?.Value ??
+            "net8.0";
+
+        return new ProjectInfo
+        {
+            Name = name,
+            Framework = framework,
+            ProjectPath = Path.GetDirectoryName(csproj)!
+        };
+    }
+}
